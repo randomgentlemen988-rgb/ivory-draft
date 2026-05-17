3be94248334be56c4e0bc9ffbfb9d82e7d6f9068
randomgentlemen988-rgb <randomgentlemen988@gmail.com>
2026-05-17 17:55:26 -0500

    Add Sentry frontend monitoring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,14 @@
+import * as Sentry from "@sentry/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  enabled: process.env.NODE_ENV === "production" && !!process.env.REACT_APP_SENTRY_DSN,
+});
 root.render(
   <React.StrictMode>
     <App />
